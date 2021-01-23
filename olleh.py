0a719b3b2bbe0805a2807838d0bb34f224f752b2
Caio <caio.forteribeiro@gmail.com>
2021-01-23 14:31:52 +0000

    Small changes to 'olleh.py'

--- olleh.py
+++ olleh.py
@@ -1,1 +1,9 @@
 print ("!dlrow ,olleH"[::-1])
+print("What's your name?") #asks for name
+myName=input()
+print("It's goood to meet you, "+ myName)
+print("The length of your name is:")
+print(len(myName))
+print("What's your age?") #asks for age
+myAge=input()
+print("You'll be "+str(int(myAge)+5)+" in five years!")
