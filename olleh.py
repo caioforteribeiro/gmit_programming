246d0e16acf777aaf1d4174a69e4a2dc5e053164
Caio <caio.forteribeiro@gmail.com>
2021-01-23 14:33:13 +0000

    fixed typo

--- olleh.py
+++ olleh.py
@@ -1,7 +1,7 @@
 print ("!dlrow ,olleH"[::-1])
 print("What's your name?") #asks for name
 myName=input()
-print("It's goood to meet you, "+ myName)
+print("It's good to meet you, "+ myName)
 print("The length of your name is:")
 print(len(myName))
 print("What's your age?") #asks for age
