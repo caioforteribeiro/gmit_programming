eb5fc304df2ca3a3f5225bcac022d202c6956ba8
Caio <caio.forteribeiro@gmail.com>
2021-02-06 14:22:30 +0000

    week03 - lab3.3

---
+++ week03/len.py
@@ -0,0 +1,7 @@
+#Gives the lenght of a string
+#Author: Caio Forte Ribeiro
+
+yourString = input("Type something here: ")
+lenght = len(yourString)
+
+print ("You've typed in {} characters.".format(lenght))
